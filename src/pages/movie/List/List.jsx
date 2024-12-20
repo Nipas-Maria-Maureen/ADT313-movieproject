308351d1a9eef621772f0ae5b260f8b7ad5c9610
Nipas-Maria-Maureen <maureennipas2@gmail.com>
2024-12-20 22:03:47 +0800

    Added New Design

--- src/pages/movie/List/List.jsx
+++ src/pages/movie/List/List.jsx
@@ -1,15 +1,15 @@
-import { useNavigate } from 'react-router-dom';
-import './List.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import "./List.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Lists = () => {
-  const accessToken = localStorage.getItem('accessToken');
+  const accessToken = localStorage.getItem("accessToken");
   const navigate = useNavigate();
   const [lists, setLists] = useState([]);
 
   const getMovies = () => {
-    axios.get('/movies').then((response) => {
+    axios.get("/movies").then((response) => {
       setLists(response.data);
     });
   };
@@ -20,7 +20,7 @@ const Lists = () => {
 
   const handleDelete = (id) => {
     const isConfirm = window.confirm(
-      'Are you sure that you want to delete this data?'
+      "Are you sure you want to delete this data?"
     );
     if (isConfirm) {
       axios
@@ -30,31 +30,29 @@ const Lists = () => {
           },
         })
         .then(() => {
-          const tempLists = [...lists];
-          const index = lists.findIndex((movie) => movie.id === id);
-          if (index !== undefined || index !== -1) {
-            tempLists.splice(index, 1);
-            setLists(tempLists);
-          }
-          getMovies();
+          setLists((prevLists) => prevLists.filter((movie) => movie.id !== id));
+          alert("Movie deleted successfully!");
+        })
+        .catch((error) => {
+          console.error("Error deleting movie:", error);
+          alert("Failed to delete the movie. Please try again.");
         });
     }
   };
 
   return (
-    <div className='lists-container'>
-      <div className='create-container'>
-        <button className='create'
-          type='button'
-          onClick={() => {
-            navigate('/main/movies/form');
-          }}
+    <div className="lists-container">
+      <div className="create-container">
+        <button
+          className="create"
+          type="button"
+          onClick={() => navigate("/main/movies/form")}
         >
-          Create new
+          Create New
         </button>
       </div>
-      <div className='table-container'>
-        <table className='movie-lists'>
+      <div className="table-container">
+        <table className="movie-lists">
           <thead>
             <tr>
               <th>ID</th>
@@ -65,18 +63,21 @@ const Lists = () => {
           <tbody>
             {lists.map((movie) => (
               <tr key={movie.id}>
-                <td>{movie.id}</td>
-                <td className="title-cell">{movie.title}</td> 
-                <td>
-                  <button className='edit'
-                    type='button'
-                    onClick={() => {
-                      navigate('/main/movies/form/' + movie.id);
-                    }}
+                <td className="id-cell">{movie.id}</td>
+                <td className="title-cell"><img src={movie.posterPath} alt="Movie Poster" className="movie-poster" />{movie.title}</td>
+                <td className="action-cell">
+                  <button
+                    className="edit"
+                    type="button"
+                    onClick={() => navigate(`/main/movies/form/${movie.id}`)}
                   >
                     Edit
                   </button>
-                  <button className='delete' type='button' onClick={() => handleDelete(movie.id)}>
+                  <button
+                    className="delete"
+                    type="button"
+                    onClick={() => handleDelete(movie.id)}
+                  >
                     Delete
                   </button>
                 </td>
